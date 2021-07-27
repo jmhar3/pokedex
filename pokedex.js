695204e7f51eb592e7554e30a410d1c153173d4e
jmhar <jmhar@protonmail.com>
2021-07-27 20:18:27 +1000

    organise code

--- pokedex.js
+++ pokedex.js
@@ -1,72 +1,104 @@
-//SEARCH
 const searchBar = document.getElementById('input');
 const searchIcon = document.getElementById('search');
-const pokedex = {}
+const pokedex = document.getElementById('pokedex');
+const card = document.getElementsByClassName('card');
+let modal = document.getElementsByClassName("modal");
+let close = document.getElementById('close');
+let caughtTally = document.getElementById('caught');
+let missingTally = document.getElementById('missing');
 
-let card
+window.addEventListener("DOMContentLoaded", () => {
+    fetchPokemonList();
+})
+
+// CLOSE MODAL BUTTON
+
+close.addEventListener("click", () => {
+    document.querySelector(".modal").classList.add("hidden");
+})
+
+// COLLECTION BUTTON
+
+let collectionButton = document.getElementById('collection-button');
+collectionButton.addEventListener("click", () => {
+    if (collectionButton.innerText === "ADD TO COLLECTION") {
+        document.querySelector('#collection-button').style.backgroundColor = 'royalblue';
+        document.getElementById('collection-button').innerText = "REMOVE FROM COLLECTION"
+    } else {
+        document.getElementById('collection-button').style.backgroundColor = '#CC0000';
+        document.getElementById('collection-button').innerText = "ADD TO COLLECTION"
+    };
+})
+
+//SEARCH
+
+// SEARCH ICON
 
 searchIcon.addEventListener('click', () => {
     searchBar.focus();
 })
 
-//POKEDEX
-document.addEventListener("DOMContentLoaded", () => {
-    // document.querySelector(".modal").classList.add("hidden");
-    fetchKantoPokemon();
+// SEARCH BAR - fix
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredPokemon = pokemon.filter(pokeData => {
+        return (
+            pokeData.name.toLowerCase().includes(searchString) || pokeData.type.toLowerCase().includes(searchString)
+        );
+    });
+    renderPokemon(filteredPokemon);
 });
 
-function fetchKantoPokemon() {
+// POKEDEX
+// const allPokemon = []
+
+// PARSE DATA TO GET ARRAY OF OBJECTS
+// use async + await to simplify?
+
+
+// FETCH POKEMON LIST INCLUDING NAME + URL
+
+const fetchPokemonList = async () => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
         .then(response => response.json())
-        .then(allpokemon => {
-            allpokemon.results.forEach(function (pokemon) {
+        .then(allPokemon => {
+            allPokemon.results.forEach(pokemon => {
                 fetchPokemonData(pokemon);
-                filteredData();
-                pokedex[pokemon.name] = pokemon
+                // filterPokemon();
             })
         })
-        .catch(function (error) {
-            alert("fetchKantoPokemon failed");
+        .catch(error => {
+            console.log("fetchPokemonList failed");
             console.log(error.message);
         })
-    console.log(pokedex)
 }
 
+// FETCH INDIVIDUAL STATS
 
-function fetchPokemonData(pokemon) {
+const fetchPokemonData = async (pokemon) => {
     let url = pokemon.url
     fetch(url)
         .then(response => response.json())
         .then(pokeData => {
-            renderPokemon(pokeData)
+            // const converted = JSON.parse(pokeData)
+            // renderPokemon(converted);
+            renderPokemon(pokeData);
         })
-    .catch(function (error) {
-        alert("fetchPokemonData failed!");
-        console.log(error.message);
-    })
+        .catch(error => {
+            console.log("fetchPokemonData failed!");
+            console.log(error.message);
+        })
 }
 
+// GENERATE POKEMON CARD INFO
 
-// function updatePokedex(pokemon) {
-//     let pokedex = document.getElementById('pokedex');
-//     removePokemon(pokedex);
-//     pokemon.forEach(poke => addPokemon(poke))
-// }
-
-// function removePokemon(element) {
-//     let child = element.lastElementChild;
-//     while (child) {
-//         element.removeChild(child);
-//         child = element.lastElementChild
-//     }
-// }
-
-function renderPokemon(pokeData) {
+const renderPokemon = async (pokeData) => {
     let allPokemonContainer = document.getElementById('pokedex');
-    let pokeContainer = document.createElement("div")
-    pokeContainer.classList.add('card');
+    let card = document.createElement("div")
+    card.classList.add('card');
 
-    createPokeImage(pokeData.id, pokeContainer);
+    createPokeImage(pokeData.id, card);
 
     let pokeName = (pokeData.name).toUpperCase();
 
@@ -80,43 +112,43 @@ function renderPokemon(pokeData) {
 
     createTypes(pokeData.types, pokeTypes)
 
-    pokeContainer.append(pokeTitle, pokeTypes);
+    card.append(pokeTitle, pokeTypes);
 
-    allPokemonContainer.appendChild(pokeContainer);
-
-    allPokemonContainer.addEventListener('click', (e) => {
-        if (e.target.classList.includes('.card')) {
-            document.querySelector(".modal").classList.remove("hidden");
-        };
-    });
+    allPokemonContainer.appendChild(card);
 }
 
+//TYPES *custom colours*
 function createTypes(types, div) {
-    types.forEach(function (type) {
+    types.forEach(type => {
         let typeDiv = document.createElement('p');
         typeDiv.innerText = type['type']['name'];
-        if (typeDiv.innerText === 'normal') { typeDiv.style.backgroundColor = "darkkhaki" }
-        else if (typeDiv.innerText === 'fire') { typeDiv.style.background = "darkorange" }
-        else if (typeDiv.innerText === 'water') { typeDiv.style.background = "dodgerblue" }
-        else if (typeDiv.innerText === 'grass') { typeDiv.style.background = "forestgreen" }
-        else if (typeDiv.innerText === 'electric') { typeDiv.style.background = "gold" }
-        else if (typeDiv.innerText === 'ice') { typeDiv.style.background = "lightblue" }
-        else if (typeDiv.innerText === 'fighting') { typeDiv.style.background = "crimson" }
-        else if (typeDiv.innerText === 'poison') { typeDiv.style.background = "lightpurple" }
-        else if (typeDiv.innerText === 'ground') { typeDiv.style.background = "darkgoldenrod" }
-        else if (typeDiv.innerText === 'flying') { typeDiv.style.background = "orchid" }
-        else if (typeDiv.innerText === 'psychic') { typeDiv.style.background = "hotpink" }
-        else if (typeDiv.innerText === 'bug') { typeDiv.style.background = "yellowgreen" }
-        else if (typeDiv.innerText === 'rock') { typeDiv.style.background = "saddlebrown" }
-        else if (typeDiv.innerText === 'ghost') { typeDiv.style.background = "grey" }
-        else if (typeDiv.innerText === 'dragon') { typeDiv.style.background = "purple" }
-        else if (typeDiv.innerText === 'dark') { typeDiv.style.background = "browngrey" }
-        else if (typeDiv.innerText === 'steel') { typeDiv.style.background = "lightgrey" }
-        else if (typeDiv.innerText === 'fairy') { typeDiv.style.background = "pink" }
+
+        switch (typeDiv.innerText) {
+            case 'normal': typeDiv.style.backgroundColor = "darkkhaki"; break;
+            case 'fire': typeDiv.style.background = "darkorange"; break;
+            case 'water': typeDiv.style.background = "dodgerblue"; break;
+            case 'grass': typeDiv.style.background = "forestgreen"; break;
+            case 'electric': typeDiv.style.background = "gold"; break;
+            case 'ice': typeDiv.style.background = "lightblue"; break;
+            case 'fighting': typeDiv.style.background = "crimson"; break;
+            case 'poison': typeDiv.style.background = "lightpurple"; break;
+            case 'ground': typeDiv.style.background = "darkgoldenrod"; break;
+            case 'flying': typeDiv.style.background = "orchid"; break;
+            case 'psychic': typeDiv.style.background = "hotpink"; break;
+            case 'bug': typeDiv.style.background = "yellowgreen"; break;
+            case 'rock': typeDiv.style.background = "saddlebrown"; break;
+            case'ghost': typeDiv.style.background = "grey"; break;
+            case 'dragon': typeDiv.style.background = "purple"; break;
+            case 'dark': typeDiv.style.background = "browngrey"; break;
+            case'steel': typeDiv.style.background = "lightgrey"; break;
+            case 'fairy': typeDiv.style.background = "pink"; break;
+        }
+
         div.append(typeDiv)
     })
 }
 
+//SOURCE IMAGES
 function createPokeImage(pokeID, containerDiv) {
     let pokeImgContainer = document.createElement('div');
 
@@ -127,58 +159,117 @@ function createPokeImage(pokeID, containerDiv) {
     containerDiv.append(pokeImgContainer);
 }
 
-function pokemonSearch(input) {
-    update
-}
+// //MODAL INFO
+// //fix
+// const renderModal = async (pokeData) => {
+//     let modal = document.getElementById('modal');
+//     let modalCard = document.createElement("div")
+//     modalCard.classList.add('modal-card');
 
-function filteredData() {
-    searchBar.addEventListener('keyup', (e) => {
-        const searchString = e.target.value.toLowerCase();
+//     createPokeImage(pokeData.id, modalCard);
 
-        const filteredPokemon = pokeData.filter((pokemon) => {
-            return (
-                pokemon.name.toLowerCase().includes(searchString) ||
-                pokemon.type.toLowerCase().includes(searchString)
-            );
-        });
-        renderPokemon(filteredPokemon);
-    });
-}
+//     let pokeName = (pokeData.name).toUpperCase();
+
+//     let pokeID = String(pokeData.id).padStart(3, '0');
+
+//     let pokeTitle = document.createElement('h2')
+//     pokeTitle.innerText = `${pokeID} ${pokeName}`
+
+//     let pokeTypes = document.createElement('div')
+//     pokeTypes.className = 'type';
+//     createTypes(pokeData.types, pokeTypes)
+
+//     let pokeAbility = document.createElement('div')
+//     pokeAbility.className = 'ability';
+//     createAbilities(pokeData.ability, pokeAbility)
+
+//     let pokeHeightWeight = document.createElement('div')
+//     pokeHeightWeight.className = 'height-weight';
+//     createHeightWeight(pokeData.height, pokeData.height, pokeHeightWeight)
+
+//     card.append(pokeTitle, pokeTypes, pokeAbility);
+
+//     modal.appendChild(card);
+// }
+
+// //ABILITY
+// function createAbilities(abilities, div) {
+//     abilities.forEach(ability => {
+//         let abilityDiv = document.createElement('h4');
+//         abilityDiv.innerText = ability['ability']['name'];
+//         div.append(abilityDiv)
+//     })
+// }
+
+// //HEIGHT WEIGHT
+// function createHeightWeight(heightWeight, div) {
+//     heightWeight.forEach((height, weight) => {
+//         let height = document.createElement('h4');
+//         height.innerText = height['height']['name'];
+//         let weight = document.createElement('h4');
+//         weight.innerText = weight['weight']['name'];
+//         div.append(height + weight)
+//     })
+// }
 
 //MODAL
-let modal = document.getElementsByClassName("modal");
-let close = document.getElementById('close')
-let collectionButton = document.getElementById('collection-button');
 
-function openModal() {
-    document.querySelector(".modal").classList.remove("hidden");
+//OPEN MODAL
 
-    collectionButton.addEventListener('click', (e) => {
-        if (e.target.innerText.includes('ADD')) {
-            addToCollection(pokeData);
-        } else (e.target.innerText.includes('REMOVE'))
-        removeFromCollection(pokeData);
-    });
+// function openModal(pokeData) {
+//     document.querySelector(".modal").classList.remove("hidden");
+// }
 
-}
+// pokedex.addEventListener('click', e => {
+//     if (e.target.className === 'card') {
+//         openModal(e.target)
+//     }
+// })
 
-close.addEventListener("click", function () {
-    document.querySelector(".modal").classList.add("hidden");
-})
+    // console.log(e.target)
+    // check target to figure out how to link modal
+
+// or
+
+// card.addEventListener('click', (e) => {
+//     if (e.target.innerText.includes(`${pokeName}`)) {
+//         document.querySelector(".modal").classList.remove("hidden");
+//     }
+// });
+
+
+
+// CREATE NEW USER
+
+// const form = document.querySelector('.user-form');
+// const feedback = document.querySelector('.feedback');
+// const username = form.username.value;
+// const usernamePattern = /^[a-zA-Z0-9]{6,20}$/;
+
+// form.addEventListener('submit', e => {
+//     e.preventDefault();
+//     if (usernamePattern.test(username)) {
+//         feedback.textContent = 'username is valid'
+//     } else {
+//         feedback.textContent = 'username not valid. must be made up of letters and/or numbers only & be 6-20 characters long'
+//     }
+// })
+
+// form.username.addEventListener('keyup', e => {
+//     if (usernamePattern.test(e.target.value)) {
+//         form.username.setAttribute('class', 'success');
+//     } else {
+//         form.username.setAttribute('class', 'error')
+//     }
+// })
+
+
 
 //CAUGHT CATALOGUE
 
-collectionButton.addEventListener("click", function () {
-    if (collectionButton.innerText == "ADD TO COLLECTION") {
-        document.querySelector('#collection-button').style.backgroundColor = 'royalblue';
-        document.getElementById('collection-button').innerText = "REMOVE FROM COLLECTION"
-    } else {
-        document.getElementById('collection-button').style.backgroundColor = '#CC0000';
-        document.getElementById('collection-button').innerText = "ADD TO COLLECTION"
-    };
-})
-
-//create user, 
+// convert api info into locally stored array
+// .map to return array with collection value to each pokemon automatically set to false
+// use .filter to show all true (caught) and all false (missing)
 
 // $(document).ready(function(){
 //     $(".addButton").on("click", function(event){
@@ -192,7 +283,32 @@ collectionButton.addEventListener("click", function () {
 //     });
 // });
 
-// let caughtTally = document.getElementById('caught')
-// let missingTally = document.getElementById('missing')
-// missingTally.innerText = `MISSING = ${missing}`;
-// caughtTally.innerText = `CAUGHT = ${collection}`;
+// missingTally.innerText = `MISSING = ${missingTally}`;
+// caughtTally.innerText = `CAUGHT = ${collectionTally}`;
+
+// forEach pokemon
+// localStorage.setItem('collection', 'caught')
+
+// let collection = localStorage.getItem('collection');
+
+//update
+// specificPokemon = localStorage.setItem('collection', 'caught')
+
+//clear
+// localStorage.clear();
+
+// const caughtPokemon = fetchPokemonList.filter(pokemon => pokemon.collection === true);
+// const caughtTally = fetchPokemonList.reduce(pokemon => pokemon.collection === true);
+
+// const missingPokemon = fetchPokemonList.filter(pokemon => pokemon.collection === false);
+// const missingTally = fetchPokemonList.reduce(pokemon => pokemon.collection === false);
+
+
+
+// CLOSE MODAL WHEN CLICKING OUTSIDE BOX
+
+// WHY DOES THIS BREAK EVERYTHING THAT COMES AFTER IT?
+// AND STILL NOT WORK?!
+// modal.addEventListener("click", () => {
+//     document.querySelector(".modal").classList.add("hidden");
+// })
---
+++ test.js
@@ -0,0 +1,41 @@
+const charactersList = document.getElementById('pokedex');
+const searchBar = document.getElementById('input');
+let hpCharacters = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
+
+const loadCharacters = async () => {
+    try {
+        const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.url}</p>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
+
+loadCharacters();
